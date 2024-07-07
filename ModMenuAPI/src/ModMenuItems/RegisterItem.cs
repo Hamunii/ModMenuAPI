cd7529227f7e65a8fc7d03ef09f65f1ac42be295
Hamunii <samuel.suni04@gmail.com>
2024-07-07 10:51:40 +0300

    remove static register item method

--- ModMenuAPI/src/ModMenuItems/RegisterItem.cs
+++ ModMenuAPI/src/ModMenuItems/RegisterItem.cs
@@ -17,6 +17,7 @@ public class ModMenu
     internal static List<ModMenuMenuItem> ModMenus { get; } = new();
 
     private readonly string m_menuTitle = null!;
+
     /// <summary>
     /// Create a menu panel instance to register menu items to, <br/>
     /// without having to specify the menu title for each item.
@@ -24,11 +25,12 @@ public class ModMenu
     /// <param name="menuTitle">The name of the menu items registered through this instance will go to.</param>
     public ModMenu(string menuTitle)
     {
-        if(menuTitle is null)
+        if (menuTitle is null)
             throw new ArgumentNullException(nameof(menuTitle), "Menu Title cannot be null!");
 
         m_menuTitle = menuTitle;
     }
+
     /// <summary>
     ///Register a menu item under the menu defined by the current instance of <c>ModMenu</c>.
     /// </summary>
@@ -36,34 +38,23 @@ public class ModMenu
     /// <returns>Current instance of <c>ModMenu</c>.</returns>
     public ModMenu RegisterItem(MMItemBase menuItem)
     {
-        RegisterItem(menuItem, m_menuTitle);
-        return this;
-    }
-
-    /// <summary>
-    /// Register a menu item under a menu with the specified name.<br/>
-    /// If a menu with the specified title doesn't exist yet, it will be created.
-    /// </summary>
-    /// <param name="menuItem">The menu item to register.</param>
-    /// <param name="menuTitle">The name of the menu this item will be listed under.</param>
-    /// <returns>The <c>menuItem</c> instance.</returns>
-    public static T RegisterItem<T>(T menuItem, string menuTitle) where T : MMItemBase
-    {
         menuItem.ParentAssembly = Assembly.GetCallingAssembly();
         // MMLog.Log($"Adding item \"{menuItem.Metadata.Name}\" from assembly: " + menuItem.ParentAssembly.FullName);
 
-        ModMenuMenuItem? menu = ModMenus.Find(x => x.MenuTitle.ToLower().Equals(menuTitle.ToLower()));
-        if(menu is not null)
+        ModMenuMenuItem? menu = ModMenus.Find(x =>
+            x.MenuTitle.ToLower().Equals(m_menuTitle.ToLower())
+        );
+        if (menu is not null)
         {
             menu.MenuItems.Add(menuItem);
         }
         else
         {
-            var newMenu = new ModMenuMenuItem(menuTitle);
+            var newMenu = new ModMenuMenuItem(m_menuTitle);
             newMenu.MenuItems.Add(menuItem);
             ModMenus.Add(newMenu);
         }
-        return menuItem;
+        return this;
     }
 
     /// <summary>
@@ -75,8 +66,10 @@ public class ModMenu
     /// <returns>Whether or not the item was found and removed or not.</returns>
     public static bool RemoveItem(string itemName, string menuTitle)
     {
-        ModMenuMenuItem? menu = ModMenus.Find(x => x.MenuTitle.ToLower().Equals(menuTitle.ToLower()));
-        if(menu is null)
+        ModMenuMenuItem? menu = ModMenus.Find(x =>
+            x.MenuTitle.ToLower().Equals(menuTitle.ToLower())
+        );
+        if (menu is null)
             return false;
 
         menu.MenuItems.RemoveAll(item => item.Metadata.Name.Equals(itemName));
@@ -89,9 +82,8 @@ public class ModMenu
     /// </summary>
     public static void RemoveAllOwnedBy(Assembly ass)
     {
-        var em = ModMenus.GetEnumerator();
-        while (em.MoveNext())
-            em.Current.MenuItems.RemoveAll(item => item.ParentAssembly.Equals(ass));
+        foreach (var menu in ModMenus)
+            menu.MenuItems.RemoveAll(item => item.ParentAssembly == ass);
     }
 
     /// <summary>
